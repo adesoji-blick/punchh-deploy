fa1400f3fbfc9c0bedce64f74319eceaa63c6bb2
Adesoji A <aiyemonase@Adesojis-MacBook-Pro.local>
2023-08-01 23:48:39 -0600

    added Jenkinsfile

---
+++ plugin.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  profile = var.profile
+  region  = var.region
+}
+
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 3.27"
+#     }
+#   }
+#   required_version = ">= 0.14.9"
+# }
